d6fe0f605f794c61fdf6e2b7786e02435ba52bd8
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-03-02 10:53:12 +0100

    add to list

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
 //połaczenie do bazy danych (host, login, hasło, baza)
 $db = new mysqli('localhost','root','','shoppingList');
 
+//sprawdź czy otrzymałeś dane z formularza
+if(isset($_REQUEST['newThing']) && $_REQUEST['newThing'] != "" ) {
+    //wysłano nową rzecz do dodania
+    echo "Dodaje do listy";
+    //składnia 1: "INSERT INTO list (id, thing) VALUES (NULL, 'ser')";
+    //składnia 2: "INSERT INTO list VALUES (NULL, ser)"; <- koniecznie podać
+    //                                                      wszystkie kolumny
+    
+    //tak nie robimy bo sql injection
+    //$thing = $_REQUEST['newThing'];
+    //$q = "INSERT INTO list VALUES (NULL, \'$thing\')";
+
+
+    //tworzy kwerendę
+    $q = $db->prepare("INSERT INTO list VALUES (NULL, ?)");
+    //podstawia wartości zamiast znaków zapytania
+    $q->bind_param('s', $_REQUEST['newThing']);
+    // 's' oznacza element tekstowy typu 'string'
+    //wywołaj kwerendę
+    $q->execute();
+}
+
 //ręcznie szykujemy kwerendę
 $q = "SELECT * FROM list";
 
@@ -22,7 +44,16 @@ while($row = $result->fetch_assoc()) {
 }
 echo '</ul>';
 
+
+?>
+<form action="index.php" method="get">
+<label for="newThing">Dodaj do listy:</label>
+<input type="text" name="newThing" id="newThing">
+<input type="submit" value="Dodaj">
+</form>
+
+<?php
 //debug, testy
 echo '<pre>';
-var_dump($row);
+var_dump($_REQUEST);
 ?>
